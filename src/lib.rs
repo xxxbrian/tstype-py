3180f89bc2576d32e3c88fa5c13d7678f8bab426
Bojin Li <bojinxx@gmail.com>
2024-03-29 19:31:00 +1100

    feat: add a function to build TS type definition from json ast tree

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use pyo3::prelude::*;
+use tstype_rs::ast::TsType;
+use tstype_rs::builder::build;
 use tstype_rs::parser::parse;
 
 /// Parses a TypeScript type definition to json.
@@ -13,9 +15,22 @@ fn parse_type_definition(input: &str) -> PyResult<String> {
     }
 }
 
+/// Builds a TypeScript type definition from json.
+#[pyfunction]
+fn build_type_definition(input: &str) -> PyResult<String> {
+    let deserialized: TsType = serde_json::from_str(input).map_err(|e| {
+        PyErr::new::<pyo3::exceptions::PyValueError, _>(format!(
+            "Failed to deserialize json: {}",
+            e
+        ))
+    })?;
+    Ok(build(deserialized))
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn tstype_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(parse_type_definition, m)?)?;
+    m.add_function(wrap_pyfunction!(build_type_definition, m)?)?;
     Ok(())
 }
